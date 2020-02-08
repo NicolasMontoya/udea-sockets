ba951836244116530d5f02dfbaa8839015b6c301
Nicolas Montoya <edward.nmontoya@gmail.com>
2020-02-08 15:42:38 -0500

    Initial package for GITAServer implementation
    Added variant of socketServer implementation for Python

---
+++ gita_socket/gita_server.py
@@ -0,0 +1,158 @@
+import threading
+import socket
+import selectors
+import sys
+
+if hasattr(selectors, 'PollSelector'):
+    _ServerSelector = selectors.PollSelector
+else:
+    _ServerSelector = selectors.SelectSelector
+
+""""
+Es te proyecto se realizó basado en la clase SocketServer librería de python. Cuando se desee trabajar con 
+Socket es ideal recorrir a Tornado o serversocket, dos librerías especializadas en dicho procedimiento.
+"""
+
+
+class GitaServer:
+
+    address_family = socket.AF_INET
+    socket_type = socket.SOCK_STREAM
+    request_queue_size = 5
+    allow_reuse_address = False
+    timeout = None
+
+    # Inicialización del servidor
+    def __init__(self, server_address, RequestHandlerClass, bind_and_activate=True):
+        self.server_address = server_address
+        self.RequestHandlerClass = RequestHandlerClass
+        self.socket = socket.socket(self.address_family, self.socket_type)
+        self.__is_shut_down = threading.Event()
+        self.__shutdown_request = False
+        if bind_and_activate:
+            try:
+                self.server_bind()
+                self.server_activate()
+            except Exception as e:
+                self.server_close()
+                raise
+
+    # Unión o conexión del socket
+    def server_bind(self):
+        if self.allow_reuse_address:
+            self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket.bind(self.server_address)
+        self.server_address = self.socket.getsockname()
+
+    # El socket comienza a escuchar
+    def server_activate(self):
+        self.socket.listen(self.request_queue_size)
+
+    def server_close(self):
+        self.socket.close()
+
+    def serve_forever(self, poll_interval=0.5):
+        self.__is_shut_down.clear()
+        try:
+            with _ServerSelector() as selector:
+                selector.register(self, selectors.EVENT_READ)
+
+                while not self.__shutdown_request:
+                    ready = selector.select(poll_interval)
+                    if self.__shutdown_request:
+                        break
+                    if ready:
+                        self._handle_request_noblock()
+
+        finally:
+            self.__shutdown_request = False
+            self.__is_shut_down.set()
+
+    def shutdown(self):
+        self.__shutdown_request = True
+        self.__is_shut_down.wait()
+
+    def _handle_request_noblock(self):
+        try:
+            request, client_address = self.get_request()
+        except OSError:
+            return
+        if self.verify_request(request, client_address):
+            try:
+                self.process_request(request, client_address)
+            except Exception:
+                self.handle_error(request, client_address)
+                self.shutdown_request(request)
+            except:
+                self.shutdown_request(request)
+                raise
+        else:
+            self.shutdown_request(request)
+
+    def handle_timeout(self):
+        pass
+
+    def verify_request(self, request, client_address):
+        return True
+
+    def process_request(self, request, client_address):
+        self.finish_request(request, client_address)
+        self.shutdown_request(request)
+
+    def finish_request(self, request, client_address):
+        self.RequestHandlerClass(request, client_address, self)
+
+    def close_request(self, request):
+        request.close()
+
+    def handle_error(self, request, client_address):
+        print('-' * 40, file=sys.stderr)
+        print('Exception happened during processing of request from',
+              client_address, file=sys.stderr)
+        import traceback
+        traceback.print_exc()
+        print('-' * 40, file=sys.stderr)
+
+    def fileno(self):
+        return self.socket.fileno()
+
+    def get_request(self):
+        return self.socket.accept()
+
+    def shutdown_request(self, request):
+        try:
+            # explicitly shutdown.  socket.close() merely releases
+            # the socket and waits for GC to perform the actual close.
+            request.shutdown(socket.SHUT_WR)
+        except OSError:
+            pass  # some platforms may raise ENOTCONN here
+        self.close_request(request)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.server_close()
+
+
+class BaseRequestHandler:
+
+    def __init__(self, request, client_address, server):
+        self.request = request
+        self.client_address = client_address
+        self.server = server
+        self.setup()
+        try:
+            self.handle()
+        finally:
+            self.finish()
+
+    def setup(self):
+        pass
+
+    def handle(self):
+        pass
+
+    def finish(self):
+        pass
+
