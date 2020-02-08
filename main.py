6ee3912781dcfc0e19836ac7384a5f125db05e4c
Nicolas Montoya <edward.nmontoya@gmail.com>
2020-02-08 15:43:12 -0500

    Added main

---
+++ main.py
@@ -0,0 +1,23 @@
+from gita_socket import gita_server
+
+
+class MyTCPHandler(gita_server.BaseRequestHandler):
+
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        print("{} wrote:".format(self.client_address[0]))
+        print(self.data)
+        # just send back the same data, but upper-cased
+        self.request.sendall(self.data.upper())
+
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9999
+
+    # Create the server, binding to localhost on port 9999
+    server = gita_server.GitaServer((HOST, PORT), MyTCPHandler)
+
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
