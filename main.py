7c633175a7aecd26c390fe8f18eef86b4135a250
Nicolas Montoya <edward.nmontoya@gmail.com>
2020-02-08 21:38:22 -0500

    Fixed server request, used packet class

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
-from gita_socket import gita_server
+from gita_socket.server import GitaServer, BaseRequestHandler
+import pickle
+from utils.packet import Packet
 
 
-class MyTCPHandler(gita_server.BaseRequestHandler):
+class MyTCPHandler(BaseRequestHandler):
 
     def handle(self):
         # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
+        data = self.request.recv(4096)
+        data_object = pickle.loads(data)
+        if data_object.type == 1:
+            data_object.response = 'co'
+        else:
+            data_object.response = 'None'
         # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())
+        self.request.sendall(pickle.dumps(data_object))
 
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 9999
 
     # Create the server, binding to localhost on port 9999
-    server = gita_server.GitaServer((HOST, PORT), MyTCPHandler)
+    server = GitaServer((HOST, PORT), MyTCPHandler)
 
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
